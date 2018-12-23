79e7e3acbe51ed2d0096001f9d2e630f11d41c2c
tomtomcoder155 <45235721+tomtomcoder155@users.noreply.github.com>
2018-12-23 04:32:56 -0700

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,16 @@
+#This will print the star startint with 1 star on line one, 10 stars on line ten,
+#and decreasing back to 1 star at line 20
+star = 0
+while star != 10:
+        print('*'* star)
+        star +=1
+while star != 0:
+        print('*'* star)
+        star -=1
+
+        
+
+                        
+
+        
+               
